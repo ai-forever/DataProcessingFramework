cff7ff51bedc5148cbd060b2cc772d3be23386db
EkaterinaVoloshina <izakladno@yandex.ru>
2023-12-14 14:40:43 +0300

    Add new aesthetic filter

---
+++ DPF/filters/images/aesthetic_improved_filter.py
@@ -0,0 +1,165 @@
+from typing import Dict, List, Union
+import os
+from urllib.request import urlretrieve
+import torch
+from torch import nn
+import clip
+
+import webdataset as wds
+from PIL import Image
+import io
+import matplotlib.pyplot as plt
+import json
+
+from warnings import filterwarnings
+
+import numpy as np
+import pytorch_lightning as pl
+import torch.nn as nn
+from torchvision import datasets, transforms
+import tqdm
+
+from os.path import join
+from datasets import load_dataset
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+
+# from PIL import Image, ImageFile, ImageDraw
+
+try:
+    from torch.utils.data.dataloader import default_collate
+except ImportError:
+    from torch.utils.data import default_collate
+
+from DPF.utils import read_image_rgb_from_bytes
+from DPF.filters.utils import identical_collate_fn
+from .img_filter import ImageFilter
+
+
+class MLP(pl.LightningModule):
+    def __init__(self, input_size, xcol='emb', ycol='avg_rating'):
+        super().__init__()
+        self.input_size = input_size
+        self.xcol = xcol
+        self.ycol = ycol
+        self.layers = nn.Sequential(
+            nn.Linear(self.input_size, 1024),
+            nn.Dropout(0.2),
+            nn.Linear(1024, 128),
+            nn.Dropout(0.2),
+            nn.Linear(128, 64),
+            nn.Dropout(0.1),
+            
+            nn.Linear(64, 16),
+
+            nn.Linear(16, 1)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+    def training_step(self, batch, batch_idx):
+            x = batch[self.xcol]
+            y = batch[self.ycol].reshape(-1, 1)
+            x_hat = self.layers(x)
+            loss = F.mse_loss(x_hat, y)
+            return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x = batch[self.xcol]
+        y = batch[self.ycol].reshape(-1, 1)
+        x_hat = self.layers(x)
+        loss = F.mse_loss(x_hat, y)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+def normalized(a, axis=-1, order=2):
+    import numpy as np  # pylint: disable=import-outside-toplevel
+
+    l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
+    l2[l2 == 0] = 1
+    return a / np.expand_dims(l2, axis)
+
+
+def get_improved_aesthetic_model(cache_folder):
+    """
+    Load the aethetic model
+    """
+    path_to_model = cache_folder + "/sac+logos+ava1-l14-linearMSE.pth" 
+    if not os.path.exists(path_to_model):
+        os.makedirs(cache_folder, exist_ok=True)
+        url_model = (
+            "https://github.com/christophschuhmann/improved-aesthetic-predictor/blob/main/"
+            + "sac+logos+ava1-l14-linearMSE.pth" 
+            + '?raw=true'
+        )
+        # TODO rework download
+        urlretrieve(url_model, path_to_model)
+        
+    model = MLP(768)  # CLIP embedding dim is 768 for CLIP ViT L 14
+
+    s = torch.load(path_to_model) 
+
+    model.load_state_dict(s)
+    model.eval()    
+    
+    return model
+
+
+class ImprovedAestheticFilter(ImageFilter):
+    """
+    AestheticFilter class
+    """
+
+    def __init__(
+        self,
+        weights_folder: str,
+        device: str = "cuda:0",
+        workers: int = 16,
+        batch_size: int = 64,
+        pbar: bool = True,
+    ):
+        super().__init__(pbar)
+
+        self.num_workers = workers
+        self.batch_size = batch_size
+        self.device = device
+
+        self.weights_folder = weights_folder
+        
+        self.aesthetic_model = get_improved_aesthetic_model(weights_folder)
+        self.aesthetic_model.to(self.device)
+        self.clip_model, self.clip_transforms = clip.load("ViT-L/14", device=device)  #RN50x64   
+
+        self.schema = [self.key_column, "improved_aesthetic_score"]
+        self.dataloader_kwargs = {
+            "num_workers": self.num_workers,
+            "batch_size": self.batch_size,
+            "drop_last": False,
+        }
+
+    def preprocess(self, modality2data: Dict[str, Union[bytes, str]], metadata: dict):
+        key = metadata[self.key_column]
+        pil_image = read_image_rgb_from_bytes(modality2data['image'])
+        
+        image = self.clip_transforms(pil_image)
+
+        return key, image
+
+    def process_batch(self, batch) -> dict:
+        df_batch_labels = self._generate_dict_from_schema()
+
+        keys, image_tensors = list(zip(*batch))
+        batch = default_collate(image_tensors).to(self.device)
+        with torch.no_grad():
+            inputs = self.clip_model.encode_image(batch)
+            inputs = normalized(inputs.cpu().detach().numpy())
+            outputs = self.aesthetic_model(torch.from_numpy(inputs).to(self.device).type(torch.cuda.FloatTensor))
+        
+        df_batch_labels["improved_aesthetic_score"].extend(outputs.cpu().reshape(-1).tolist())
+        df_batch_labels[self.key_column].extend(keys)
+
+        return df_batch_labels
