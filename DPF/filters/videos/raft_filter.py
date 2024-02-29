e69ba99ae8f33c8f99d7b0c2d0fc6090d48d9674
Sofia Kirillova <zshakhin@sberbank.ru>
2024-02-29 11:54:55 +0300

    fix: move frame transforms to dataloader and add param to pass n frames

--- DPF/filters/videos/raft_filter.py
+++ DPF/filters/videos/raft_filter.py
@@ -1,5 +1,5 @@
 import io
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Tuple
 
 import cv2
 import imageio.v3 as iio
@@ -32,7 +32,16 @@ class InputPadder:
         ht, wd = x.shape[-2:]
         c = [self._pad[2], ht - self._pad[3], self._pad[0], wd - self._pad[1]]
         return x[..., c[0]:c[1], c[2]:c[3]]
-
+    
+    
+def transform_frame(frame: np.ndarray, target_size: Tuple, device: str):
+    frame = cv2.resize(frame, dsize=(target_size[0], target_size[1]), interpolation=cv2.INTER_LINEAR)
+    frame = torch.from_numpy(frame).permute(2, 0, 1).float()[None]
+    
+    padder = InputPadder(frame.shape)
+    frame = padder.pad(frame)[0]
+    return frame 
+    
 
 class RAFTOpticalFlowFilter(VideoFilter):
     """ 
@@ -50,6 +59,7 @@ class RAFTOpticalFlowFilter(VideoFilter):
     """ 
     
     def __init__(self,
+                 pass_frames: int = 10,
                  weights_path: str = "raft-things.pth",
                  small: bool = False,
                  device: str = "cuda:0",
@@ -61,6 +71,8 @@ class RAFTOpticalFlowFilter(VideoFilter):
         self.num_workers = workers
         self.batch_size = batch_size
         self.device = device
+        
+        self.pass_frames = pass_frames
         
         self.model = torch.nn.DataParallel(RAFT(small=small))
         self.model.load_state_dict(torch.load(weights_path))
@@ -79,23 +91,18 @@ class RAFTOpticalFlowFilter(VideoFilter):
             "batch_size": self.batch_size,
             "drop_last": False,
         }
-    
-    def load_image(self, image_file):
-        image_file = np.array(image_file).astype(np.uint8)
-        if image_file.shape[0] > image_file.shape[1]:
-            image_file = cv2.resize(image_file, (450, 800))
-        elif image_file.shape[1] > image_file.shape[0]:
-            image_file = cv2.resize(image_file, (800, 450))
-        else:
-            image_file = cv2.resize(image_file, (450, 450))
-        image_file = torch.from_numpy(image_file).permute(2, 0, 1).float()
-        return image_file[None].to(self.device)
 
     def preprocess(self, modality2data: Dict[str, Union[bytes, str]], metadata: dict):
         key = metadata[self.key_column]
         video_file = modality2data['video']
         
         frames = iio.imread(io.BytesIO(video_file), plugin="pyav")
+        if frames.shape[1] > frames.shape[2]:
+            frames = [transform_frame(frame=i, target_size=(450, 800), device=self.device) for i in frames]
+        elif frames.shape[2] > frames.shape[1]:
+            frames = [transform_frame(frame=i, target_size=(800, 450), device=self.device) for i in frames]
+        else:
+            frames = [transform_frame(frame=i, targe_size=(450, 450), device=self.device) for i in frames]
         return key, frames
         
     def process_batch(self, batch) -> dict:
@@ -105,14 +112,14 @@ class RAFTOpticalFlowFilter(VideoFilter):
         for data in batch:
             key, frames = data
             with torch.no_grad():
-                for current_frame, next_frame in zip(frames[:-1], frames[1:]):
-                    current_frame = self.load_image(current_frame)
-                    next_frame = self.load_image(next_frame)
-
-                    padder = InputPadder(current_frame.shape)
-                    current_frame, next_frame = padder.pad(current_frame, next_frame)
-
-                    _, flow = self.model(current_frame, next_frame, iters=20, test_mode=True)
+                for i in range(self.pass_frames, len(frames), self.pass_frames):
+                    current_frame = frames[i - self.pass_frames]
+                    next_frame = frames[i]
+                    
+                    _, flow = self.model(current_frame.to(self.device),
+                                         next_frame.to(self.device),
+                                         iters=20, test_mode=True)
+                    
                     flow = flow.detach().cpu().numpy()
                     magnitude, angle = cv2.cartToPolar(flow[0][..., 0], flow[0][..., 1])
                     mean_magnitudes.append(magnitude)
