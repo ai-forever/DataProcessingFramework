be2906eef12ca57064feb95bb93af6251c644f54
leffff <levnovitskiy@gmail.com>
2024-07-10 19:19:01 +0300

    correct types in image_utils.py

--- DPF/utils/image_utils.py
+++ DPF/utils/image_utils.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from PIL import Image
 
 
-def read_image_rgb(path: str, force_rgb: bool = True) -> Image.Image:
+def read_image_rgb(path: str, force_rgb: bool = True) -> Image.ImageFile:
     pil_img = Image.open(path)
     pil_img.load()  # type: ignore
     if pil_img.format == "PNG" and pil_img.mode != "RGBA":
@@ -13,7 +13,7 @@ def read_image_rgb(path: str, force_rgb: bool = True) -> Image.Image:
     return pil_img
 
 
-def read_image_rgb_from_bytes(img_bytes: bytes, force_rgb: bool = True) -> Image.Image:
+def read_image_rgb_from_bytes(img_bytes: bytes, force_rgb: bool = True) -> Image.ImageFile:
     pil_img = Image.open(BytesIO(img_bytes))
     pil_img.load()  # type: ignore
     if pil_img.format == "PNG" and pil_img.mode != "RGBA":
