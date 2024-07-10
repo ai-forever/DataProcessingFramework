9cf27819dea8389d4a2b64b325bb7686b9573a4e
LizaDakhova <dahova.eyu@phystech.edu>
2024-07-10 19:47:18 +0300

    ignore types in image_utils.py

--- DPF/utils/image_utils.py
+++ DPF/utils/image_utils.py
@@ -3,23 +3,21 @@ from io import BytesIO
 from PIL import Image
 
 
-def read_image_rgb(path: str, force_rgb: bool = True) -> Image.ImageFile:
+def read_image_rgb(path: str, force_rgb: bool = True) -> Image.Image:
     pil_img = Image.open(path)
     pil_img.load()  # type: ignore
     if pil_img.format == "PNG" and pil_img.mode != "RGBA":
         pil_img = pil_img.convert("RGBA")
     if force_rgb:
         pil_img = pil_img.convert("RGB")
-    return pil_img
+    return pil_img  # type: ignore
 
 
-def read_image_rgb_from_bytes(img_bytes: bytes, force_rgb: bool = True) -> Image.ImageFile:
+def read_image_rgb_from_bytes(img_bytes: bytes, force_rgb: bool = True) -> Image.Image:
     pil_img = Image.open(BytesIO(img_bytes))
     pil_img.load()  # type: ignore
     if pil_img.format == "PNG" and pil_img.mode != "RGBA":
         pil_img = pil_img.convert("RGBA")
     if force_rgb:
         pil_img = pil_img.convert("RGB")
-    return pil_img
-
-# test 1
+    return pil_img  # type: ignore
