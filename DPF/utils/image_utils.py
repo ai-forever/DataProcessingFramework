cfa0774018e787e8829ddbd6403ed4136c788e5f
LizaDakhova <dahova.eyu@phystech.edu>
2024-07-11 16:08:55 +0300

    update types

--- DPF/utils/image_utils.py
+++ DPF/utils/image_utils.py
@@ -7,17 +7,17 @@ def read_image_rgb(path: str, force_rgb: bool = True) -> Image.Image:
     pil_img = Image.open(path)
     pil_img.load()  # type: ignore
     if pil_img.format == "PNG" and pil_img.mode != "RGBA":
-        pil_img = pil_img.convert("RGBA")
+        pil_img = pil_img.convert("RGBA")  # type: ignore
     if force_rgb:
-        pil_img = pil_img.convert("RGB")
-    return pil_img  # type: ignore
+        pil_img = pil_img.convert("RGB")  # type: ignore
+    return pil_img
 
 
 def read_image_rgb_from_bytes(img_bytes: bytes, force_rgb: bool = True) -> Image.Image:
     pil_img = Image.open(BytesIO(img_bytes))
     pil_img.load()  # type: ignore
     if pil_img.format == "PNG" and pil_img.mode != "RGBA":
-        pil_img = pil_img.convert("RGBA")
+        pil_img = pil_img.convert("RGBA")  # type: ignore
     if force_rgb:
-        pil_img = pil_img.convert("RGB")
-    return pil_img  # type: ignore
+        pil_img = pil_img.convert("RGB")   # type: ignore
+    return pil_img
